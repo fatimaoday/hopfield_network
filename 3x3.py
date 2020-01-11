1c7c63e48b891c6816662728d32fafec15d361b8
Fatima Oday <fatmaoa98@gmail.com>
2020-01-11 18:39:52 +0300

    find letter function

--- 3x3.py
+++ 3x3.py
@@ -18,11 +18,15 @@ data = [[1,0,1, #U
 
         [1,1,1, #T
          0,1,0,
-         0,1,0],
+         0,1,0]
 
-        [1,1,1, #C
-         1,0,0,
-         1,1,1]
+        # [1,1,1, #C
+        #  1,0,0,
+        #  1,1,1]
+
+        #  [1,0,1, #X
+        #   0,1,0,
+        #   1,0,1]
         
        ]
 
@@ -51,7 +55,6 @@ print (w)
 
 
 ######################################################
-
 #Testing stage
 
 input_sample =[1,1,1, 
@@ -107,22 +110,31 @@ while ( not (all(x[i] == y[i] for i in range(size))) ):
     iteration +=1
 
 
+######################################################
+#Finding the letter 
 
-# counter_matrix = np.zeros((len(data),), dtype=int)
+def find_letter (y):
+    counter_matrix = np.zeros((len(data),), dtype=int)
 
-# for index1 ,i in enumerate(data):
-#     for index2 ,j in enumerate(i) :
-#         if j == -1:
-#             data[index1][index2] = 0
+    for index1 ,i in enumerate(data):
+        for index2 ,j in enumerate(i) :
+            if j == -1:
+                data[index1][index2] = 0
 
 
-# for index_i,i in enumerate(data) :
-#     for index_j,j in enumerate(i):
-#         if (j == y[index_j]):
-#             counter_matrix[index_i] +=1
-    
+    for index_i,i in enumerate(data) :
+        for index_j,j in enumerate(i):
+            if (j == y[index_j]):
+                counter_matrix[index_i] +=1
+        
 
-# boo = np.where(counter_matrix == max(counter_matrix))
-# #print(boo[0][0])
-# print("your letter is:")
-# print(alphabet[boo[0][0]])
+    letter = np.where(counter_matrix == max(counter_matrix))
+    letter2 = letter[0][0]
+    letter3 = alphabet[letter2]
+
+    print("your letter is:")
+    print(letter3[0:3])
+    print(letter3[3:6])
+    print(letter3[6:9])
+
+find_letter(y)
